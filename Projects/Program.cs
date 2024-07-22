451d84cd6e9c36ecef6ffec69a11a469a993ae61
ayanada739 <ayanada739@gmail.com>
2024-07-22 17:39:45 +0300

    Third Project [ Duration ]

--- Projects/Program.cs
+++ Projects/Program.cs
@@ -1,5 +1,6 @@
 ﻿using static Assignment.Projects.FirstProject;
 using static Assignment.Projects.SecondProject;
+using static Assignment.Projects.ThirdProject;
 
 namespace Projects
 {
@@ -73,7 +74,49 @@ namespace Projects
 
             #endregion
 
+            #region Third Project Duration
 
+            //Duration D1 = new Duration(1, 10, 15);
+            //Console.WriteLine(D1.ToString());
+
+            //Duration D2 = new Duration(7800);
+            //Console.WriteLine(D2.ToString());
+
+            //Duration D3 = new Duration(666);
+            //Console.WriteLine(D3.ToString());
+
+            //D3 = D1 + D2;
+            //Console.WriteLine(D3.ToString());
+
+            //D3 = D1 + 7800;
+            //Console.WriteLine(D3.ToString());
+
+            //D3 = 666 + D3;
+            //Console.WriteLine(D3.ToString());
+
+            //D3 = ++D1;
+            //Console.WriteLine(D3.ToString());
+
+            //D3 = --D2;
+            //Console.WriteLine(D3.ToString());
+
+            //D1 = D1 - D2;
+            //Console.WriteLine(D1.ToString());
+
+            //if (D1 > D2)
+            //{
+            //    Console.WriteLine("D1 is greater than D2");
+            //}
+            //else if (D1 <= D2)
+            //{
+            //    Console.WriteLine("D1 is less than or equal to D2");
+            //}
+
+            //DateTime dt = (DateTime)D1;
+            //Console.WriteLine("DateTime: " + dt.ToString());
+
+
+            #endregion
 
 
 
