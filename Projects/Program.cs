8c99becb8f04741a3f4f721f23ad15d12ed890d6
ayanada739 <ayanada739@gmail.com>
2024-07-22 17:09:40 +0300

    First Project  [ Point3D Class ]

---
+++ Projects/Program.cs
@@ -0,0 +1,68 @@
+﻿using static Projects.FirstProject;
+
+namespace Projects
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            #region First Project  Point3D Class
+
+            //try
+            //{
+            //    Console.Write("Enter coordinates for P1 (x y z): ");
+            //    string[] input1 = Console.ReadLine().Split();
+            //    int x1 = int.Parse(input1[0]);
+            //    int y1 = int.Parse(input1[1]);
+            //    int z1 = int.Parse(input1[2]);
+
+            //    Console.Write("Enter coordinates for P2 (x y z): ");
+            //    string[] input2 = Console.ReadLine().Split();
+            //    int x2 = int.Parse(input2[0]);
+            //    int y2 = int.Parse(input2[1]);
+            //    int z2 = int.Parse(input2[2]);
+
+            //    Point3D P1 = new Point3D(x1, y1, z1);
+            //    Point3D P2 = new Point3D(x2, y2, z2);
+
+
+            //    Console.WriteLine(P1.ToString());
+            //    Console.WriteLine(P2.ToString());
+
+            //    if (P1 == P2)
+            //    {
+            //        Console.WriteLine("P1 and P2 are equal.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("P1 and P2 are not equal.");
+            //    }
+
+            //    Point3D[] points = { P1, P2, new Point3D(5, 5, 5), new Point3D(1, 2, 3) };
+            //    Array.Sort(points);
+
+            //    Console.WriteLine("Sorted points:");
+            //    foreach (var point in points)
+            //    {
+            //        Console.WriteLine(point.ToString());
+            //    }
+
+            //    Point3D P1Clone = (Point3D)P1.Clone();
+            //    Console.WriteLine("P1 Clone: " + P1Clone.ToString());
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Invalid input: " + ex.Message);
+            //}
+
+            #endregion
+
+
+
+
+
+        }
+    }
+}
