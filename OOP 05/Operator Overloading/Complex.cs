50f5ecc421a89ef9acffe8051eae9daf882d59dd
ayanada739 <ayanada739@gmail.com>
2024-07-22 18:58:05 +0300

    Operators Overloading [ Binary Operators ]

---
+++ OOP 05/Operator Overloading/Complex.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_05.Operator_Overloading
+{
+    internal class Complex
+    {
+
+        public int Real { get; set; }
+        public int Imag { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{Real} + {Imag}i";
+        }
+
+
+        #region Operators Overloading
+        // +
+        // Non — Private , Class member Method
+
+
+        public static Complex operator +(Complex Left, Complex Right)
+        {
+            return new Complex()
+            {
+
+                Real =(Left?.Real ?? 0) + Right?.Real ?? 0,
+                Imag =(Left?.Imag ?? 0) + Right?.Imag ?? 0
+            };
+        }
+
+
+        public static Complex operator -(Complex Left, Complex Right)
+        {
+            return new Complex()
+            {
+
+                Real = (Left?.Real ?? 0) - Right?.Real ?? 0,
+                Imag = (Left?.Imag ?? 0) - Right?.Imag ?? 0
+            };
+        }
+        #endregion
+    }
+}
--- OOP 05/Program.cs
+++ OOP 05/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace OOP_05
+﻿using OOP_05.Operator_Overloading;
+
+namespace OOP_05
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Operator Overloading
+
+
+            Complex C1 = new Complex() { Real = 3, Imag = 5 };
+            Console.WriteLine(C1); //3+5i
+            Complex C2 = new Complex() { Real = 2, Imag = 4 };   
+            Console.WriteLine(C2); //2+4i
+
+            #region Binary Operators Overloading
+            //////Complex C3 = C1 + C2; // Invalid
+            //////Console.WriteLine("===============");
+            //////Console.WriteLine(C3); // 5 + 9i
+
+            ////Complex C3 = C1 - C2;  
+            ////Console.WriteLine(C3); 
+
+            //Complex C3 = default;
+            //C3 += C1; //C3 = NULL + C1
+            //Console.WriteLine(C3);
+            //C3 -= C1;//C3 = C3 - C1 
+            #endregion
+
+
+            #endregion
         }
     }
 }
